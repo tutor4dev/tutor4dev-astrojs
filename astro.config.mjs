a265e51337de5bd1db8968e8f4a3c8b10b766743
Kongthap Thammachat <jeud@hotmail.com>
2025-12-02 20:45:33 +0700

    add Google Analytics 251202

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
 import path from 'node:path'
 import { defineConfig } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
+import partytown from '@astrojs/partytown'
 
 import { remarkInclude } from './src/lib/plugin-remark.mjs'
 import { rehypeBulmaTable, rehypeBulmaTitle, rehypeBulmaImage, rehypeBulmaYoutube } from './src/lib/plugin-rehype.mjs'
 
 export default defineConfig({
   site: 'https://tutor4dev.com',
-  integrations: [sitemap()],
+  integrations: [
+    sitemap(),
+    partytown({ config: { forward: ['dataLayer.push'] } })
+  ],
   vite: {
     resolve: {
       alias: {
